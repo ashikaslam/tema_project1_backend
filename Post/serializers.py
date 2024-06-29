86daaf6ccad6b8707293135766cb471b017d0bf3
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-06-29 16:17:30 +0600

    123

--- Post/serializers.py
+++ Post/serializers.py
@@ -4,7 +4,7 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Post
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer): # 1
     class Meta:
         model = Post
         fields = ['text','photo']
--- Post/views.py
+++ Post/views.py
@@ -16,7 +16,8 @@ from rest_framework.authentication import SessionAuthentication
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from . import models
-class Post_view(APIView):
+
+class Post_view(APIView): # 1
     authentication_classes=[JWTAuthentication,SessionAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = serializers.PostSerializer
@@ -30,6 +31,7 @@ class Post_view(APIView):
             if text or photo:
                 post = models.Post.objects.create(photo=photo,text=text,owner_profile=user.profile)
                 if post: return Response({"message": "post creation completed", "status": 1}, status=status.HTTP_200_OK)
+            return Response({'error': "unknown errors", "status": 0}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
